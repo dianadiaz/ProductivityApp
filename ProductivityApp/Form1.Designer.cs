e7b0eaa9233cbfe4abf5794281ae29e8113ff8ba
Joel Gilbert <gilbert.279@gmail.com>
2016-11-22 20:21:01 -0600

    Generated Tabs on Form1.cs

--- ProductivityApp/Form1.Designer.cs
+++ ProductivityApp/Form1.Designer.cs
@@ -28,12 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ProductivityApp = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.ProductivityApp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ProductivityApp
+            // 
+            this.ProductivityApp.AccessibleDescription = "ProductivityApp";
+            this.ProductivityApp.AccessibleName = "ProductivityApp";
+            this.ProductivityApp.Controls.Add(this.tabPage1);
+            this.ProductivityApp.Controls.Add(this.tabPage2);
+            this.ProductivityApp.Controls.Add(this.tabPage3);
+            this.ProductivityApp.Controls.Add(this.tabPage4);
+            this.ProductivityApp.Controls.Add(this.tabPage5);
+            this.ProductivityApp.Controls.Add(this.tabPage6);
+            this.ProductivityApp.Location = new System.Drawing.Point(13, 13);
+            this.ProductivityApp.Name = "ProductivityApp";
+            this.ProductivityApp.SelectedIndex = 0;
+            this.ProductivityApp.Size = new System.Drawing.Size(1240, 620);
+            this.ProductivityApp.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.AccessibleDescription = "Inventory";
+            this.tabPage1.AccessibleName = "Inventory";
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1232, 594);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Inventory";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.AccessibleDescription = "TimeSheet";
+            this.tabPage2.AccessibleName = "TimeSheet";
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1232, 594);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Time Sheet";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.AccessibleDescription = "AddressBook";
+            this.tabPage3.AccessibleName = "AddressBook";
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1232, 594);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Address Book";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.AccessibleDescription = "Tasks";
+            this.tabPage4.AccessibleName = "Tasks";
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1232, 594);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Tasks";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.AccessibleDescription = "Budget";
+            this.tabPage5.AccessibleName = "Budget";
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(1232, 594);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Budget";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.AccessibleDescription = "Calculator";
+            this.tabPage6.AccessibleName = "Calculator";
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(1232, 594);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Calculator";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1265, 645);
+            this.Controls.Add(this.ProductivityApp);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ProductivityApp.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl ProductivityApp;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
     }
 }
 
