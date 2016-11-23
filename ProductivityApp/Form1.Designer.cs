e0b38f7a6ba10d990c78b0c0102c0d40dcf178f9
Joel Gilbert <gilbert.279@gmail.com>
2016-11-22 20:42:58 -0600

    Updated Tab Names

--- ProductivityApp/Form1.Designer.cs
+++ ProductivityApp/Form1.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.ProductivityApp = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.tabPage5 = new System.Windows.Forms.TabPage();
-            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.InventoryTab = new System.Windows.Forms.TabPage();
+            this.TimeSheet = new System.Windows.Forms.TabPage();
+            this.AddressBook = new System.Windows.Forms.TabPage();
+            this.Tasks = new System.Windows.Forms.TabPage();
+            this.Budget = new System.Windows.Forms.TabPage();
+            this.Calculator = new System.Windows.Forms.TabPage();
             this.ProductivityApp.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,89 +42,89 @@
             // 
             this.ProductivityApp.AccessibleDescription = "ProductivityApp";
             this.ProductivityApp.AccessibleName = "ProductivityApp";
-            this.ProductivityApp.Controls.Add(this.tabPage1);
-            this.ProductivityApp.Controls.Add(this.tabPage2);
-            this.ProductivityApp.Controls.Add(this.tabPage3);
-            this.ProductivityApp.Controls.Add(this.tabPage4);
-            this.ProductivityApp.Controls.Add(this.tabPage5);
-            this.ProductivityApp.Controls.Add(this.tabPage6);
+            this.ProductivityApp.Controls.Add(this.InventoryTab);
+            this.ProductivityApp.Controls.Add(this.TimeSheet);
+            this.ProductivityApp.Controls.Add(this.AddressBook);
+            this.ProductivityApp.Controls.Add(this.Tasks);
+            this.ProductivityApp.Controls.Add(this.Budget);
+            this.ProductivityApp.Controls.Add(this.Calculator);
             this.ProductivityApp.Location = new System.Drawing.Point(13, 13);
             this.ProductivityApp.Name = "ProductivityApp";
             this.ProductivityApp.SelectedIndex = 0;
             this.ProductivityApp.Size = new System.Drawing.Size(1240, 620);
             this.ProductivityApp.TabIndex = 0;
             // 
-            // tabPage1
+            // InventoryTab
             // 
-            this.tabPage1.AccessibleDescription = "Inventory";
-            this.tabPage1.AccessibleName = "Inventory";
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1232, 594);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Inventory";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.InventoryTab.AccessibleDescription = "Inventory";
+            this.InventoryTab.AccessibleName = "Inventory";
+            this.InventoryTab.Location = new System.Drawing.Point(4, 22);
+            this.InventoryTab.Name = "InventoryTab";
+            this.InventoryTab.Padding = new System.Windows.Forms.Padding(3);
+            this.InventoryTab.Size = new System.Drawing.Size(1232, 594);
+            this.InventoryTab.TabIndex = 0;
+            this.InventoryTab.Text = "Inventory";
+            this.InventoryTab.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // TimeSheet
             // 
-            this.tabPage2.AccessibleDescription = "TimeSheet";
-            this.tabPage2.AccessibleName = "TimeSheet";
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1232, 594);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Time Sheet";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.TimeSheet.AccessibleDescription = "TimeSheet";
+            this.TimeSheet.AccessibleName = "TimeSheet";
+            this.TimeSheet.Location = new System.Drawing.Point(4, 22);
+            this.TimeSheet.Name = "TimeSheet";
+            this.TimeSheet.Padding = new System.Windows.Forms.Padding(3);
+            this.TimeSheet.Size = new System.Drawing.Size(1232, 594);
+            this.TimeSheet.TabIndex = 1;
+            this.TimeSheet.Text = "Time Sheet";
+            this.TimeSheet.UseVisualStyleBackColor = true;
             // 
-            // tabPage3
+            // AddressBook
             // 
-            this.tabPage3.AccessibleDescription = "AddressBook";
-            this.tabPage3.AccessibleName = "AddressBook";
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(1232, 594);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Address Book";
-            this.tabPage3.UseVisualStyleBackColor = true;
+            this.AddressBook.AccessibleDescription = "AddressBook";
+            this.AddressBook.AccessibleName = "AddressBook";
+            this.AddressBook.Location = new System.Drawing.Point(4, 22);
+            this.AddressBook.Name = "AddressBook";
+            this.AddressBook.Padding = new System.Windows.Forms.Padding(3);
+            this.AddressBook.Size = new System.Drawing.Size(1232, 594);
+            this.AddressBook.TabIndex = 2;
+            this.AddressBook.Text = "Address Book";
+            this.AddressBook.UseVisualStyleBackColor = true;
             // 
-            // tabPage4
+            // Tasks
             // 
-            this.tabPage4.AccessibleDescription = "Tasks";
-            this.tabPage4.AccessibleName = "Tasks";
-            this.tabPage4.Location = new System.Drawing.Point(4, 22);
-            this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(1232, 594);
-            this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "Tasks";
-            this.tabPage4.UseVisualStyleBackColor = true;
+            this.Tasks.AccessibleDescription = "Tasks";
+            this.Tasks.AccessibleName = "Tasks";
+            this.Tasks.Location = new System.Drawing.Point(4, 22);
+            this.Tasks.Name = "Tasks";
+            this.Tasks.Padding = new System.Windows.Forms.Padding(3);
+            this.Tasks.Size = new System.Drawing.Size(1232, 594);
+            this.Tasks.TabIndex = 3;
+            this.Tasks.Text = "Tasks";
+            this.Tasks.UseVisualStyleBackColor = true;
             // 
-            // tabPage5
+            // Budget
             // 
-            this.tabPage5.AccessibleDescription = "Budget";
-            this.tabPage5.AccessibleName = "Budget";
-            this.tabPage5.Location = new System.Drawing.Point(4, 22);
-            this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage5.Size = new System.Drawing.Size(1232, 594);
-            this.tabPage5.TabIndex = 4;
-            this.tabPage5.Text = "Budget";
-            this.tabPage5.UseVisualStyleBackColor = true;
+            this.Budget.AccessibleDescription = "Budget";
+            this.Budget.AccessibleName = "Budget";
+            this.Budget.Location = new System.Drawing.Point(4, 22);
+            this.Budget.Name = "Budget";
+            this.Budget.Padding = new System.Windows.Forms.Padding(3);
+            this.Budget.Size = new System.Drawing.Size(1232, 594);
+            this.Budget.TabIndex = 4;
+            this.Budget.Text = "Budget";
+            this.Budget.UseVisualStyleBackColor = true;
             // 
-            // tabPage6
+            // Calculator
             // 
-            this.tabPage6.AccessibleDescription = "Calculator";
-            this.tabPage6.AccessibleName = "Calculator";
-            this.tabPage6.Location = new System.Drawing.Point(4, 22);
-            this.tabPage6.Name = "tabPage6";
-            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage6.Size = new System.Drawing.Size(1232, 594);
-            this.tabPage6.TabIndex = 5;
-            this.tabPage6.Text = "Calculator";
-            this.tabPage6.UseVisualStyleBackColor = true;
+            this.Calculator.AccessibleDescription = "Calculator";
+            this.Calculator.AccessibleName = "Calculator";
+            this.Calculator.Location = new System.Drawing.Point(4, 22);
+            this.Calculator.Name = "Calculator";
+            this.Calculator.Padding = new System.Windows.Forms.Padding(3);
+            this.Calculator.Size = new System.Drawing.Size(1232, 594);
+            this.Calculator.TabIndex = 5;
+            this.Calculator.Text = "Calculator";
+            this.Calculator.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -143,12 +143,12 @@
         #endregion
 
         private System.Windows.Forms.TabControl ProductivityApp;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.TabPage tabPage4;
-        private System.Windows.Forms.TabPage tabPage5;
-        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage InventoryTab;
+        private System.Windows.Forms.TabPage TimeSheet;
+        private System.Windows.Forms.TabPage AddressBook;
+        private System.Windows.Forms.TabPage Tasks;
+        private System.Windows.Forms.TabPage Budget;
+        private System.Windows.Forms.TabPage Calculator;
     }
 }
 
